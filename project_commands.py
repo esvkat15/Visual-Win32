c80bfa85c678a3d87a5644a2006fd8b6b3ab0041
esvkat15 <matthewhmunoz@gmail.com>
2017-03-30 15:56:39 -0700

    listed commands
    made actual list of commands

--- project_commands.py
+++ project_commands.py
@@ -1,7 +1,23 @@
 import sublime
 import sublime_plugin
 
-class TestCommand(sublime_plugin.WindowCommand):
+class CompileCommand(sublime_plugin.WindowCommand):
+
+  def is_enabled(self):
+    return True
+
+  def run(self):
+    self.window.run_command("save_all")
+
+class RunCommand(sublime_plugin.WindowCommand):
+
+  def is_enabled(self):
+    return True
+
+  def run(self):
+    self.window.run_command("save_all")
+
+class AsmOutCommand(sublime_plugin.WindowCommand):
 
   def is_enabled(self):
     return True
