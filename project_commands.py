ae3440b3c1c2698996f62fe29ae277becf92865a
esvkat15 <matthewhmunoz@gmail.com>
2017-04-03 10:16:45 -0800

    tried fixing spacing typos
    tried fixing spacing typos

--- project_commands.py
+++ project_commands.py
@@ -3,33 +3,41 @@ import sublime_plugin
 
 class RunCommand(sublime_plugin.WindowCommand):
 
-  def is_enabled(self):
-    return True
+	def is_enabled(self):
 
-  def run(self):
-    self.window.run_command("save_all")
+		return True
+
+	def run(self):
+
+		self.window.run_command("save_all")
+
 
 class CompileCommand(sublime_plugin.WindowCommand):
 
-  def is_enabled(self):
-    return True
+	def is_enabled(self):
 
-  def run(self):
-    self.window.run_command("save")
+		return True
+
+	def run(self):
+
+		self.window.run_command("save")
+
 
 class AsmOutCommand(sublime_plugin.WindowCommand):
 
-  def is_enabled(self):
-    filename = None
-    if self.window.active_view():
-      filename = self.window.active_view().file_name()
-    if filename is None or not filename.endswith(".c"):
-      filename = None
-    return True#filename is not None
+	def is_enabled(self):
 
-  def run(self):
+		filename = None
+		if self.window.active_view():
+			filename = self.window.active_view().file_name()
+		if filename is None or not filename.endswith(".c"):
+			filename = None
+		return filename is not None
 
-  	cmd = ["C:\\Windows\\System32\\sublime\\cl.bat"]
-  	cmd += ["/?"]
-    self.window.run_command("save")
-    self.window.run_command("exec", {"cmd": cmd, "file_regex": "(?i)^(?:ERROR: |WARNING: )[^C-Z]*([C-Z]:[^:]*):([0-9]+):([0-9]*)(.*)$"})
+	def run(self):
+
+		cmd = ["C:\\Windows\\System32\\sublime\\cl.bat"]
+		cmd += ["/?"]
+		self.window.run_command("save")
+		self.window.run_command("exec", {"cmd": cmd, "file_regex": "(?i)^(?:ERROR: |WARNING: )[^C-Z]*([C-Z]:[^:]*):([0-9]+):([0-9]*)(.*)$"})
+
