e16d424706fcb67fca717c731dd5406869908aa0
esvkat15 <matthewhmunoz@gmail.com>
2017-04-04 11:53:19 -0800

    added compile command
    added compile command

--- project_commands.py
+++ project_commands.py
@@ -1,15 +1,23 @@
 import sublime
 import sublime_plugin
 
+command_path = "C:\\Windows\\System32\\sublime\\"
+flags = ["/c"]
+sflags = ["/Fa"]
+
+def ifext(ext, view):
+
+	return view and view.filename() is not None and view.filename.endswith(ext)
+
 class ToExeCommand(sublime_plugin.WindowCommand):
 
 	def is_enabled(self):
 
-		return True
+		return ifext(".c", self.window.active_view()) or ifext(".asm", self.window.active_view())
 
 	def run(self):
 
-		self.window.run_command("save_all")
+		self.window.run_command("save_all") #taskkill /IM "path\main.exe" 2> nul
 
 
 class ToObjCommand(sublime_plugin.WindowCommand):
@@ -21,25 +29,24 @@ class ToObjCommand(sublime_plugin.WindowCommand):
 	def run(self):
 
 		self.window.run_command("save")
+		if ifext(".c", self.window.active_view()):
+			c = 'c'
+		if ifext(".asm", self.window.active_view()):
+			c = 'm'
+		if c:
+			cmd = [command_path + c + "l.bat"] + flags + [self.window.active_view().file_name()]
 
 
 class ToAsmCommand(sublime_plugin.WindowCommand):
 
 	def is_enabled(self):
 
-		filename = None
-		if self.window.active_view():
-			filename = self.window.active_view().file_name()
-		if filename is None or not filename.endswith(".c"):
-			filename = None
-		return filename is not None
+		return ifext(".c", self.window.active_view())
 
 	def run(self):
 
-		path = "C:\\Windows\\System32\\sublime\\"
-		filename = self.window.active_view().file_name()
-		flags = ["/c", "/Fa"]
-		cmd = [path + "cl.bat"] + flags + [filename, "&", path + "subl.exe", filename.replace(".c",".asm")]
 		self.window.run_command("save")
+		filename = self.window.active_view().file_name()
+		cmd = [command_path + "cl.bat"] + flags + sflags + [filename, "&", command_path + "subl.exe", filename.replace(".c", ".asm")]
 		self.window.run_command("exec", {"cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
 
