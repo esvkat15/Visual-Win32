798715828a7afe53a5eac932b94fd59c2d868239
esvkat15 <matthewhmunoz@gmail.com>
2017-04-05 12:14:23 -0800

    testing file paths
    testing file paths

--- project_commands.py
+++ project_commands.py
@@ -19,9 +19,11 @@ class ToExeCommand(sublime_plugin.WindowCommand):
 	def run(self):
 
 		self.window.run_command("save_all") #taskkill /IM "path\main.exe" 2> nul
-		cmd = ["tasklist", "/v", "|", "echo"]
+		filename = self.window.active_view().file_name()
+		t = os.path.split(filename)[0] + '\\' + "new.txt"
+		cmd = ["tasklist", "/v" > temp]
 		self.window.run_command("exec", {"cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
-		sublime.error_message(os.path.split(self.window.active_view().file_name())[0])
+		sublime.error_message(temp)
 		#with open(os.path.split(self.window.active_view().file_name())[0]) as f:
 		#	sublime.error_message(f.readline())
 
