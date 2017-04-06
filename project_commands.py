70dd64e83275360a2415fc4d53c39932cbd0ed66
esvkat15 <matthewhmunoz@gmail.com>
2017-04-05 22:38:53 -0800

    testing shell
    testing shell

--- project_commands.py
+++ project_commands.py
@@ -21,10 +21,10 @@ class ToExeCommand(sublime_plugin.WindowCommand):
 		self.window.run_command("save_all") #taskkill /IM "path\main.exe" 2> nul
 		filename = self.window.active_view().file_name()
 		t = os.path.split(filename)[0] + '\\' + "new.txt"
-		cmd = ["cmd", "\"tasklist", "/v", ">", t + "\""]
-		self.window.run_command("exec", {"cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
+		cmd = ["tasklist", "/v", ">", t]
+		self.window.run_command("exec", {"shell_cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
 		with open(t) as f:
-			sublime.error_message(f.readline())
+			sublime.error_message(f.readline() + f.readline())
 
 
 class ToObjCommand(sublime_plugin.WindowCommand):
