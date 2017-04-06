97b5a8af43cc20065a97ca75e9411ca55e4ab201
esvkat15 <matthewhmunoz@gmail.com>
2017-04-05 22:31:32 -0800

    testing doublequotes for cmd ception
    lol

--- project_commands.py
+++ project_commands.py
@@ -21,7 +21,7 @@ class ToExeCommand(sublime_plugin.WindowCommand):
 		self.window.run_command("save_all") #taskkill /IM "path\main.exe" 2> nul
 		filename = self.window.active_view().file_name()
 		t = os.path.split(filename)[0] + '\\' + "new.txt"
-		cmd = ["cmd", "tasklist", "/v", ">", t]
+		cmd = ["cmd", "\"tasklist", "/v", ">", t + "\""]
 		self.window.run_command("exec", {"cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
 		with open(t) as f:
 			sublime.error_message(f.readline())
