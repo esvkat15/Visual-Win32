e8da91f9ddddaff17050b09a9a53ef8c114c28bc
esvkat15 <matthewhmunoz@gmail.com>
2017-04-06 18:28:01 -0800

    rm kill cmd
    efrefhhrbf

--- project_commands.py
+++ project_commands.py
@@ -54,8 +54,8 @@ class ToExeCommand(sublime_plugin.WindowCommand):
 	def run(self):
 
 		self.window.run_command("save_all")
-		cmd = ["taskkill", "/IM", "main.exe", "2>", "nul"]
-		window.run_command("exec", {"shell_cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
+		# cmd = ["taskkill", "/IM", "main.exe", "2>", "nul"]
+		# window.run_command("exec", {"shell_cmd": cmd, "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$"})
 
 
 
